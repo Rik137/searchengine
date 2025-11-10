5cc9883385696f68c932dd5b1eddbcdd00b9707c
Sergey Nazarov <ric19902002@icloud.com>
2025-11-10 07:57:46 +0000

    Update DefaultController.java

--- src/main/java/searchengine/controllers/DefaultController.java
+++ src/main/java/searchengine/controllers/DefaultController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DefaultController {
 
-    /**
-     * Метод формирует страницу из HTML-файла index.html,
-     * который находится в папке resources/templates.
-     * Это делает библиотека Thymeleaf.
-     */
+   /**
+   * This method generates a page from the HTML file `index.html`,
+   * located in the `resources/templates` directory.
+   * The Thymeleaf library handles this process.
+   */
     @RequestMapping("/")
     public String index() {
         return "index";
