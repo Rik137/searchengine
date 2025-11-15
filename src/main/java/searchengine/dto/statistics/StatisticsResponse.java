95e0ae9924a2a9c5cb1adbac6c66bd791b0695c3
Sergey Nazarov <ric19902002@icloud.com>
2025-11-15 06:56:31 +0000

    Update StatisticsResponse.java

--- src/main/java/searchengine/dto/statistics/StatisticsResponse.java
+++ src/main/java/searchengine/dto/statistics/StatisticsResponse.java
@@ -3,19 +3,19 @@ package searchengine.dto.statistics;
 import lombok.Data;
 
 /**
-* API response containing indexing statistics.
+* API response containing indexing statistics
 */
 
 @Data
 public class StatisticsResponse {
 
     /**
-    * Result of the request execution (true means success).
+    * Result of the request execution (true means success)
     */
     private boolean result;
 
     /**
-    * Statistical data.
+    * Statistical data
     */
     private StatisticsData statistics;
 }
